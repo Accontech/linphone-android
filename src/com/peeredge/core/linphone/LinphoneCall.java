5dac19073535abd22b16016660b034d0f3ad0231
Samson Sargsyan <samson@accontech.com>
2017-03-26 19:10:58 +0400

    Linphone call object to general call object part 1

--- src/com/peeredge/core/linphone/LinphoneCall.java
+++ src/com/peeredge/core/linphone/LinphoneCall.java
@@ -2,66 +2,83 @@ package com.peeredge.core.linphone;
 
 import com.peeredge.core.common.ListenerInterfaces.CallEvents;
 import com.peeredge.core.common.ModelInterfaces.Call;
+import com.peeredge.core.common.Models.CallState;
 import com.peeredge.core.common.Models.CallType;
+
+import org.linphone.LinphoneManager;
+import org.linphone.core.LinphoneCallStats;
+import org.linphone.core.LinphoneCore;
+import org.linphone.core.LinphoneCoreListener;
+import org.linphone.core.LinphoneCoreListenerBase;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by samson on 3/25/17.
  */
 public class LinphoneCall implements Call{
-    org.linphone.core.LinphoneCall nativeCall;
+    private org.linphone.core.LinphoneCall nativeCall;
+    private int mCallState;
+    private boolean mIsIncoming;
+    private volatile Set<CallEvents.CallStateListener> listeners = new HashSet<>();
+
     public LinphoneCall(org.linphone.core.LinphoneCall call)
     {
         nativeCall = call;
+        LinphoneCore lc = LinphoneManager.getLc();
+        lc.addListener();
+
     }
 
     @Override
     public String getID() {
-        return null;
+        return nativeCall.getCallLog().getCallId();
     }
 
     @Override
     public CallType getType() {
-        return null;
+        return CallType.LINPHONE;
     }
 
     @Override
     public boolean isVideoEnabled() {
-        return false;
+        return nativeCall.getVideoStats().equals(LinphoneCallStats.MediaType.Video) ;
     }
 
     @Override
     public boolean isOnHold() {
-        return false;
+        return mCallState == CallState.ONHOLD;
     }
 
     @Override
     public boolean isActive() {
-        return false;
+        return mCallState == CallState.CONNECTING || mCallState == CallState.ACTIVE;
     }
 
     @Override
     public boolean isIncoming() {
-        return false;
+        return mIsIncoming;
     }
 
     @Override
     public boolean isTerminated() {
-        return false;
+         return mCallState == CallState.DISCONNECTED || mCallState == CallState.DISCONNECTING || mCallState == CallState.INVALID;;
     }
 
     @Override
     public String getRemoteCallNumber() {
-        return null;
+        return nativeCall.getRemoteContact();
     }
 
     @Override
     public String getRemoteDisplay() {
-        return null;
+        return nativeCall.getRemoteContact();
     }
 
     @Override
     public int getCallState() {
-        return 0;
+        return mCallState;
     }
 
     @Override
@@ -103,5 +120,17 @@ public class LinphoneCall implements Call{
     public void sendDtmf(char digit) {
 
     }
+
+
+
+    class CallEventLisener extends LinphoneCoreListenerBase
+    {
+        @Override
+        public void callState(LinphoneCore lc, org.linphone.core.LinphoneCall call, org.linphone.core.LinphoneCall.State state, String message) {
+            if(call != nativeCall)
+                return;
+        }
+    }
+
 
 }
